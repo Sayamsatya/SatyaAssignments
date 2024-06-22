b5b93086a9b57b871b35eaa153472121f783d881
satya <satya@SatyaGitiVasu>
2024-06-22 18:18:46 +0800

    commit

--- Day6Assignment/src/Construtor/Child.java
+++ Day6Assignment/src/Construtor/Child.java
@@ -2,27 +2,26 @@ package Construtor;
 
 	public class Child extends Parent {
 
-	    public Child() {
-	        this(12);
+		Child() {
+	        this(10);
 	        System.out.println("Child default constructor");
 	    }
-	    
-	    public Child(int a) {
-	        this(12, 13);
+
+	    Child(int a) {
+	        this(a, a); 
 	        System.out.println("Child one parameterized constructor");
 	    }
-	    
-	    public Child(int a, int b) {
-	        super(12, 13, 14);  // Use the parent three parameterized constructor
+
+	    Child(int a, int b) {
+	        this(a, b, b); 
 	        System.out.println("Child two parameterized constructor");
 	    }
-	    
-	    public Child(int a, int b, int c) {
-	        this();
+
+	    Child(int a, int b, int c) {
+	        super(a, b, c); 
 	        System.out.println("Child three parameterized constructor");
 	    }
-	    
 	    public static void main(String[] args) {
-	        Child c = new Child(12, 12, 11);
+	        Child c = new Child(); 
 	    }
 }
--- Day6Assignment/src/Construtor/Parent.java
+++ Day6Assignment/src/Construtor/Parent.java
@@ -3,22 +3,22 @@ package Construtor;
 public class Parent {
 
 	 public Parent() {
-	    	
-	        System.out.println("Parent default constructor");
-	    }
+		 
+		        System.out.println("Parent default constructor");
+		    }
 
-	    public Parent(int a) {
-	        this();
-	        System.out.println("Parent one parameterized constructor");
-	    }
+		    Parent(int a) {
+		        this(); 
+		        System.out.println("Parent one parameterized constructor");
+		    }
 
-	    public Parent(int a, int b) {
-	        this(12);
-	        System.out.println("Parent two parameterized constructor");
-	    }
+		    Parent(int a, int b) {
+		        this(a); 
+		        System.out.println("Parent two parameterized constructor");
+		    }
 
-	    public Parent(int a, int b, int c) {
-	        this(12, 13);
-	        System.out.println("Parent three parameterized constructor");
-	    }
+		    Parent(int a, int b, int c) {
+		        this(a, b);
+		        System.out.println("Parent three parameterized constructor");
+		    }
 }
